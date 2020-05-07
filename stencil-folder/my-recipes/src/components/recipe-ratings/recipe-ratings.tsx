5e259977e8359f0bcc98b08ae74b512924053858
Anne-Kathrin <annekdrotleff@gmail.com>
2020-05-07 19:03:51 +0200

    deleted example

--- stencil-folder/my-recipes/src/components/recipe-ratings/recipe-ratings.tsx
+++ stencil-folder/my-recipes/src/components/recipe-ratings/recipe-ratings.tsx
@@ -40,8 +40,6 @@ export class RecipeRatings implements ComponentInterface {
           onButtonClickCancel = {(e:CustomEvent)=> this.closePopup()}
         ></add-new-rating>
         <div ref={(el)=> this.ratingBox = el as HTMLElement}>
-          <my-rating headline="Nice Recipe!" rate="Well done!"></my-rating>
-          <my-rating headline="Not good at all" rate="I tried that recepie several times but ist was just not good"></my-rating>
         </div>
       
         <div id="addrating" onClick={() => {this.showPopup()}}>
